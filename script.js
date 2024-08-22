d65f665b5885244304a5c03f90f60d143acfa89e
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-22 17:02:12 +0200

    adding funtion

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 // ESERCIZIO 1
 
 // Raccolgo elementi dal DOM
-// Creo la funzione
 // Recupero la parola scritta dall'utente
 // Splitto ogni signola lettera della parola
 // Faccio un reverse delle lettere
-// Verifico se la parola è palindroma
+// Creo la funzione e verifico se la parola è palindroma
 // Stampo in pagina il risultato
 
 // Recupero gli elementi dal DOM
-const userWord = document.getElementById('word-field');
+const userWordField = document.getElementById('word-field');
 const palButton = document.getElementById('pal-button');
 const palResult = document.getElementById('pal-result');
 
-//
+
+// Recupero la parola scritta dall'utente
+palButton.addEventListener('click', function() {
+    userWord = userWordField.value.trim();
+
+    mirroredWord(userWord);
+    console.log(userWord);
+
+})
+
+// Creo funzione che specchia la parola
+function mirroredWord(userWord) {
+    //return userWord.split('').reverse().join('');
+
+    for (let i = userWord.length - 1; i <= 0; i--) {
+        userWord.charAt(i);
+    }
+    return userWord;
+}
+
+
+
 
 
 
