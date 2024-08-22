55c70a0331058da4463ede9db1e656799a17d34f
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-22 15:15:00 +0200

    es1 comments lineup

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // ESERCIZIO 1
 
+//Raccolgo elementi dal DOM
+//Recupero la parola scritta dall'utente
+// Splitto ogni signola lettera della parola
+// Faccio un reverse delle lettere
+// Verifico se la parola è palindroma
+// Stampo in pagina il risultato
+
 // ESERCIZIO 2
