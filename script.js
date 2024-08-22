a06cb8d496d31e838d98416f1450a79a7aef2137
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-22 15:25:08 +0200

    es1 fixed comments

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // ESERCIZIO 1
 
-//Raccolgo elementi dal DOM
-//Recupero la parola scritta dall'utente
+// Raccolgo elementi dal DOM
+// Creo la funzione
+// Recupero la parola scritta dall'utente
 // Splitto ogni signola lettera della parola
 // Faccio un reverse delle lettere
 // Verifico se la parola è palindroma
 // Stampo in pagina il risultato
+
+// Recupero gli elementi dal DOM
+const userWord = document.getElementById('word-field');
+const palButton = document.getElementById('pal-button');
+const palResult = document.getElementById('pal-result');
+
+//
+
+
+
 
 // ESERCIZIO 2
